c38acc732744f7297a09fb92d0a18f924ccfb4db
Saif <74458012+SaifOmar@users.noreply.github.com>
2024-02-14 16:30:04 +0200

    changes to db and made base html

--- ITTHELPER/Server/models.py
+++ ITTHELPER/Server/models.py
@@ -5,16 +5,26 @@ from django.db import models
 # many to many relations 
 
 class CareerPath(models.Model):
-    Path = models.TextChoices()
+    CareerPath_CHOICES = [
+        ('path1', 'Path 1'),
+        ('path2', 'Path 2'),
+        # ... other choices
+    ]
+    CompanyName = models.CharField(max_length=50, choices=CareerPath_CHOICES)
 
 class EventsAndWorkshops(models.Model):
-    event = models.CharField(max_lenght=100)
+    event = models.CharField(max_length=100)
     EventTime = models.TimeField()
-    Eventplace = models.CharField(max_lenght = 100)
+    Eventplace = models.CharField(max_length = 100)
     EventPath = models.ManyToManyField(CareerPath, blank = False , related_name= "EventsAndWorkshops")
 
 class Company(models.Model):
-    CompanyName = models.TextChoices()
+    COMPANY_CHOICES = [
+        ('company1', 'Company 1'),
+        ('company2', 'Company 2'),
+        # ... other choices
+    ]
+    CompanyName = models.CharField(max_length=50, choices=COMPANY_CHOICES)
 
 class Training(models.Model):
     TrainingName = models.CharField(max_length=100)
@@ -27,11 +37,11 @@ class Training(models.Model):
 class Course(models.Model):
     CourseName = models.CharField(max_length=100)
     Path = models.ManyToManyField(CareerPath, blank = False, related_name="Courses")
-    RelatedEvents = models.ManyToManyFieldy(EventsAndWorkshops, blank= True, related_name="Courses")
+    RelatedEvents = models.ManyToManyField(EventsAndWorkshops, blank= True, related_name="Courses")
     
 
 class Jobs(models.Model):
-    JobName = models.CharField()
+    JobName = models.CharField(max_length=100)
     JobDiscribtion = models.TextField()
     JobTraining = models.ManyToManyField(Training)
     JobAssociations = models.ManyToManyField(CareerPath)
